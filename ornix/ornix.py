a42f82dca4a163129d3be372ac3177339bdbd80a
kil9 <krieiter@gmail.com>
2017-04-24 04:20:05 +0900

    Wrapping up

--- ornix/ornix.py
+++ ornix/ornix.py
@@ -1,23 +1,34 @@
 import json
+import requests
 import random
 from flask import render_template
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
+
+URL='https://hooks.slack.com/services/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'
+
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
+@app.route('/event', methods=['POST'])
+def event():
+    token = request.form['body']['challenge']
+    return token
+
 @app.route('/api', methods=['POST'])
 def api():
-    msg = { "attachments": [ {
-                "title": "닉시코",
-                "pretext": "포맷이 복잡해서",
-                "text": "다이스 *귀찮고*",
-                "mrkdwn_in": [ "text", "pretext" ] } ] }
-    encoded = json.dumps(msg)
-    return encoded
+    msg = request.form['text']
+
+    headers = {'Content-type': 'application/json'}
+    raw_data = {"text": msg}
+    encoded = json.dumps(raw_data)
+    requests.post(URL, headers=headers, data=encoded)
+
+    return ''
 
 if __name__ == '__main__':
     app.run(port=20000, debug=True)
--- web.sh
+++ web.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-APP_NAME='ornix'
-APP_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd ${APP_HOME}/ornix
-echo "${APP_NAME} started"
-uwsgi -H ${APP_HOME}/venv -w ornix:app --http :21000 -p 4 --py-auto-reload=3
+source venv/bin/activate
+gunicorn ornix:app --chdir=ornix -w 2 -b ':21000' --reload
